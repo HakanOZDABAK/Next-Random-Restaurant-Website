e444155fdb56cacc66320992d36e6adaf5ac642e
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-03-18 10:32:24 +0300

    project initialized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "primereact/resources/themes/bootstrap4-light-blue/theme.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
