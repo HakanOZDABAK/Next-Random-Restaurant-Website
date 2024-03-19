d72d6c8ff6ba1e9a5d85aa0e2f41e2f03451fac9
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-03-19 11:14:13 +0300

    MealCard redesigned

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
+import "primereact/resources/themes/viva-light/theme.css";
 import "/node_modules/primeflex/primeflex.css"
 const inter = Inter({ subsets: ["latin"] });
 
--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -5,20 +5,16 @@ import { Card } from 'primereact/card';
 
 export default function MealCard(props: { meal: any }) {
     const { meal } = props; 
-    const router = useRouter()
-
-
-
-
+  
     const footer = (
         <>
-            <Button onClick={()=>{alert("Ordered")}} label="Order This Meal" icon="pi pi-check" />
+            <Button rounded severity="info" text raised  onClick={()=>{alert("Ordered")}} label="Order This Meal" icon="pi pi-check" />
         </>
     );
 
     return (
         <div className="card flex justify-content-center">
-            <Card key={meal.idMeal} title={meal.strMeal} subTitle="Category" footer={footer} className="md:w-25rem">
+            <Card key={meal.idMeal} title={meal.strMeal} subTitle="Meal" footer={footer} className="md:w-25rem">
             <img className='h-10rem w-10rem' alt="Card" src={meal.strMealThumb} />
                 <p className="m-0">
                   You can order meal with button
