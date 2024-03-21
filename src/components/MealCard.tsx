c6a8b71ad400ff7ba31a90c941d32ed46b0dd190
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-03-21 15:05:17 +0300

    route to meal review

--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -9,18 +9,22 @@ import { MenuItem } from 'primereact/menuitem';
 export default function MealCard(props: { meal: any }) {
     const { meal } = props; 
     const {setCartItems} = useCartStore()
-    const itemRenderer = (item: MenuItem) => (
+    const router = useRouter();
+        const itemRenderer = (item: MenuItem) => (
         <a className="flex align-items-center p-menuitem-link">
           <span className="mx-2">{item.label} = {item.cost}</span>
           {item.badge && <Badge className="ml-auto" value={item.badge} />}
         </a>
       );
-
+const handleToRoute=(path:string)=>
+{
+router.push(path)
+}
       
     const footer = (
         <div className='grid'>
             <div className="col-6"> 
-            <Button rounded severity="success" text raised  onClick={()=>{setCartItems(meal.strMeal,itemRenderer)}} label="Order This Meal" icon="pi pi-check" />
+            <Button rounded severity="success" text raised  onClick={()=>{handleToRoute(`/category/${meal.strMeal}/review`)}} label="Order This Meal" icon="pi pi-check" />
             </div>
             <div className="col-6"> 
             <Button rounded severity="info" text raised  onClick={()=>{setCartItems(meal.strMeal,itemRenderer)}} label="Reviews This Food" icon="pi pi-send" />
